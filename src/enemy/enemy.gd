1c2493295ddbedfddeadf3c53a4d9b988a2e19cd
Josh McHugh <joshua.mchugh01@gmail.com>
2023-12-10 00:31:12 -0500

    Moved bullet into player directory & added enemy_bullet scene

--- src/enemy/enemy.gd
+++ src/enemy/enemy.gd
@@ -4,6 +4,7 @@ signal died
 
 var start_pos = Vector2.ZERO
 var speed = 0
+var bullet_scene = preload("res://src/enemy/enemy_bullet.tscn")
 
 @onready var screensize = get_viewport_rect().size
 
@@ -35,6 +36,9 @@ func _on_move_timer_timeout():
 	speed = randf_range(75, 100)
 
 func _on_shoot_timer_timeout():
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start(position)
 	$ShootTimer.wait_time = randf_range(4, 20)
 	$ShootTimer.start()
 
--- src/main.gd
+++ src/main.gd
@@ -15,7 +15,7 @@ func spawn_enemies():
 	for x in range(9):
 		for y in range(3):
 			var e = enemy.instantiate()
-			var pos = Vector2(x * (16 + 8) + 24, 16 * 4 + y * 17)
+			var pos = Vector2(x * (16 + 8) + 24, 16 * 4 + y * 16)
 			add_child(e)
 			e.start(pos)
 			e.died.connect(_on_enemy_died)
